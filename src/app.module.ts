756e490fc96a171314c4eca0925fb474e4fe803c
justynadanielas <justiyandere@gmail.com>
2023-03-05 15:29:46 +0100

    Configure graphql and typeorm modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuotesModule } from './quotes/quotes.module';
+import { ApolloDriver } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Quote } from './quotes/entities/quote.entity';
+
 
 @Module({
-  imports: [QuotesModule],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    driver: ApolloDriver,
+  }), 
+  TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: ':memory:',
+    entities: [Quote],  
+    synchronize: true,
+  }),
+  QuotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
