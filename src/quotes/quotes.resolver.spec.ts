7d4b4315d47440b9f61161cdbe23c61f97207902
justynadanielas <justiyandere@gmail.com>
2023-03-12 17:47:42 +0100

    Add test for quotes entity

--- src/quotes/quotes.resolver.spec.ts
+++ src/quotes/quotes.resolver.spec.ts
@@ -1,17 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Instrument } from '../instruments/entities/instrument.entity';
 import { InstrumentsService } from '../instruments/instruments.service';
+import { Quote } from './entities/quote.entity';
 import { QuotesResolver } from './quotes.resolver';
 import { QuotesService } from './quotes.service';
 
 describe('QuotesResolver', () => {
   let resolver: QuotesResolver;
 
-  const mockQuotesService = {
+  const instruments: Instrument[] = [
+    {
+      id: 1,
+      ticker_name: 'AAAA'
+    }, {
+      id: 2,
+      ticker_name: 'BBBB'
+    }
+  ]
 
+  const quotes: Quote[] = [
+    {
+      id: 1,
+      time_stamp: new Date().toISOString(),
+      price: 100,
+      intrumentId: 1,
+      instrument: {
+        id: 1,
+        ticker_name: 'AAAA'
+      }
+    }, {
+      id: 2,
+      time_stamp: new Date().toISOString(),
+      price: 100,
+      intrumentId: 2,
+      instrument: {
+        id: 2,
+        ticker_name: 'BBBB'
+      }
+    }
+  ]
+
+  const mockQuotesService = {
+    create: jest.fn(quoteInput => {
+      return Promise.resolve({
+        id: 1,
+        ...quoteInput
+      })
+    }),
+
+    findAll: jest.fn(() => {
+      return quotes;
+    }),
+
+    findOne: jest.fn((id) => {
+      return quotes[id];
+    })
   }
 
   const mockInstrumentsService = {
+    findOneByName: jest.fn(ticker_name => {
+      const result = instruments.find(el => el.ticker_name === ticker_name);
+      return Promise.resolve(result);
+    }),
 
+    create: jest.fn(instrumentInput => {
+      return Promise.resolve({
+        id: 1,
+        ...instrumentInput
+      })
+    }),
   }
 
   beforeEach(async () => {
@@ -31,4 +88,33 @@ describe('QuotesResolver', () => {
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
+
+  it('should create a quote', async () => {
+    expect(await resolver.createQuote({time_stamp: new Date().toISOString(), price: 100, intrumentId: 1})).toEqual({
+      id: expect.any(Number),
+      time_stamp: expect.any(String),
+      price: 100,
+      intrumentId: 1
+    })
+  });
+
+  it('should create a quote with given ticker', async () => {
+    expect(await resolver.createQuoteWithTicker({time_stamp: new Date().toISOString(), price: 100, intrumentTickerName: 'AAAA'})).toEqual({
+      id: expect.any(Number),
+      time_stamp: expect.any(String),
+      price: 100,
+      intrumentId: instruments.find(el => el.ticker_name == 'AAAA').id
+    })
+  });
+
+  it('should return all quotes', async () => {
+    const result = await resolver.findAll();
+    expect(result.length).toEqual(2);
+  })
+
+  it('should return a quote by given id', async () => {
+    const result = await resolver.findOne(1);
+    expect(result).toEqual(quotes[1]);
+  })
+
 });
--- src/quotes/quotes.service.spec.ts
+++ src/quotes/quotes.service.spec.ts
@@ -8,12 +8,63 @@ import { QuotesService } from './quotes.service';
 describe('QuotesService', () => {
   let service: QuotesService;
 
-  const mockQuotesRepository = {
+  const instruments: Instrument[] = [
+    {
+      id: 1,
+      ticker_name: 'AAAA'
+    }, {
+      id: 2,
+      ticker_name: 'BBBB'
+    }
+  ]
 
+  const quotes: Quote[] = [
+    {
+      id: 1,
+      time_stamp: new Date().toISOString(),
+      price: 100,
+      intrumentId: 1,
+      instrument: {
+        id: 1,
+        ticker_name: 'AAAA'
+      }
+    }, {
+      id: 2,
+      time_stamp: new Date().toISOString(),
+      price: 100,
+      intrumentId: 2,
+      instrument: {
+        id: 2,
+        ticker_name: 'BBBB'
+      }
+    }
+  ]
+
+  const mockQuotesRepository = {
+    create: jest.fn(quoteInput => {
+      return {
+        id: 1,
+        ...quoteInput
+      }
+    }),
+
+    save: jest.fn(quote => {
+      return Promise.resolve(quote);
+    }),
+
+    find: jest.fn(() => {
+      return quotes;
+    }),
+
+    findOneByOrFail: jest.fn(({id: id}) => {
+      return quotes.find((el) => el.id === id);
+    }),
   }
 
   const mockInstrumentsService = {
-
+    findOne: jest.fn((id) => {
+      return instruments[id];
+    })
   }
 
   beforeEach(async () => {
@@ -33,4 +84,29 @@ describe('QuotesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a quote', async () => {
+    expect(await service.create({time_stamp: new Date().toISOString(), price: 100, intrumentId: 1})).toEqual({
+      id: expect.any(Number),
+      time_stamp: expect.any(String),
+      price: 100,
+      intrumentId: 1
+    })
+  })
+
+  it('should return all quotes', async () => {
+    const result = await service.findAll();
+    expect(result.length).toEqual(2);
+  })
+
+  it('should return a quote by given id', async () => {
+    const result = await service.findOne(1);
+    expect(result).toEqual(quotes[0]);
+  })
+
+  it('should return an instrument by given instrument id', async () => {
+    const result = await service.getInstrument(1);
+    expect(result).toEqual(instruments[1]);
+  })
+
 }); 
