d115b43a759c45bf8b29dd7cb56dcb8a480426a2
justynadanielas <justiyandere@gmail.com>
2023-03-12 17:19:15 +0100

    Add test for instruments entity

--- src/instruments/instruments.resolver.spec.ts
+++ src/instruments/instruments.resolver.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Instrument } from './entities/instrument.entity';
 import { InstrumentsResolver } from './instruments.resolver';
 import { InstrumentsService } from './instruments.service';
 
 describe('InstrumentsResolver', () => {
   let resolver: InstrumentsResolver;
 
-  const mockInstrumentsService = {
+  const instruments: Instrument[] = [
+    {
+      id: 1,
+      ticker_name: 'AAAA'
+    }, {
+      id: 2,
+      ticker_name: 'BBBB'
+    }
+  ]
 
+  const mockInstrumentsService = {
+    create: jest.fn(instrumentInput => {
+      return {
+        id: 1,
+        ...instrumentInput
+      }
+    }),
+
+    findAll: jest.fn(() => {
+      return instruments;
+    }),
+
+    findOne: jest.fn((id) => {
+      return instruments[id];
+    })
   }
 
   beforeEach(async () => {
@@ -20,5 +44,22 @@ describe('InstrumentsResolver', () => {
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
+
+  it('should create an instrument', () => {
+    expect(resolver.createInstrument({ticker_name: 'AAAA'})).toEqual({
+      id: expect.any(Number),
+      ticker_name: 'AAAA'
+    })
+  });
+
+  it('should return all instruments', async () => {
+    const result = await resolver.findAll();
+    expect(result.length).toEqual(2);
+  })
+
+  it('should return an instrument by given id', async () => {
+    const result = await resolver.findOne(1);
+    expect(result).toEqual(instruments[1]);
+  })
 
 });
--- src/instruments/instruments.service.spec.ts
+++ src/instruments/instruments.service.spec.ts
@@ -6,8 +6,46 @@ import { InstrumentsService } from './instruments.service';
 describe('InstrumentsService', () => {
   let service: InstrumentsService;
 
+  const instruments: Instrument[] = [
+    {
+      id: 1,
+      ticker_name: 'AAAA'
+    }, {
+      id: 2,
+      ticker_name: 'BBBB'
+    }
+  ]
+
   const mockInstrumentsRepository = {
-    
+    create: jest.fn(instrumentInput => {
+      return {
+        id: 1,
+        ...instrumentInput
+      }
+    }),
+
+    save: jest.fn(instrument => {
+      return Promise.resolve(instrument);
+    }),
+
+    find: jest.fn(() => {
+      return instruments;
+    }),
+
+    findOneByOrFail: jest.fn(({id: id}) => {
+      return instruments.find((el) => el.id === id);
+    }),
+
+    findOneBy: jest.fn(ticker_name_obj => {
+      const result = instruments.find(el => el.ticker_name === ticker_name_obj.ticker_name);
+      return result;
+    }),
+
+    // findOneByAlt: jest.fn(({ticker_name}) => {
+    //   const result = instruments.find(el => el.ticker_name === ticker_name);
+    //   return result;
+    // })
+
   }
 
   beforeEach(async () => {
@@ -24,4 +62,27 @@ describe('InstrumentsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create an instrument', async () => {
+    expect(await service.create({ticker_name: 'AAAA'})).toEqual({
+      id: expect.any(Number),
+      ticker_name: 'AAAA'
+    })
+  })
+
+  it('should return all instruments', async () => {
+    const result = await service.findAll();
+    expect(result.length).toEqual(2);
+  })
+
+  it('should return an instrument by given id', async () => {
+    const result = await service.findOne(1);
+    expect(result).toEqual(instruments[0]);
+  })
+
+  it('should return an instrument by given name', async () => {
+    const result = await service.findOneByName('AAAA');
+    expect(result).toEqual(instruments[0]);
+  })
+
 });
